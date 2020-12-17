b07e2612e69c7379a8f8e4225419aa551283cd36
Shubham Narendra Fursule <47754888+shubhamfursule@users.noreply.github.com>
2020-12-17 18:39:23 +0530

    Spyder

---
+++ Closest given number.py
@@ -0,0 +1,13 @@
+from random import randint
+
+def closest(L,n):
+    max1=L[0]
+    for i in range(1,len(L)):
+        if L[i]>max1 and L[i]<n:
+            max1=L[i]
+    return max1
+        
+L1=[randint(0,30) for i in range(int(8))]
+print(L1)
+n1=int(input("Please Enter number that you want to get the closest number:  "))
+print(f"\"{closest(L1,n1)}\" is the Closest number of {n1}" )
